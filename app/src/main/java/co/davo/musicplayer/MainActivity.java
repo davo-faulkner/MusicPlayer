88bfc0f9414bd7b40715dc0650ba0f3c1ce35198
Davo Faulkner <d@davo.co>
2016-09-13 17:26:27 -0500

    Add OnClickListeners to MainActivity.java

--- app/src/main/java/co/davo/musicplayer/MainActivity.java
+++ app/src/main/java/co/davo/musicplayer/MainActivity.java
@@ -2,6 +2,8 @@ package co.davo.musicplayer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button playButton = (Button) findViewById(R.id.play_button);
+        playButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        Button pauseButton = (Button) findViewById(R.id.pause_button);
+        pauseButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
     }
 }
